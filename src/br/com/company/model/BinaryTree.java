67598eccab52bba2a68bed6b8ada7f0e3b0068c5
Francisco <campos.junior.ifrn@gmail.com>
2019-10-10 22:33:11 -0300

    Nodechar criado

---
+++ src/br/com/company/model/BinaryTree.java
@@ -0,0 +1,18 @@
+package br.com.company.model;
+
+public class BinaryTree {
+
+    NodeChar node;
+
+    public void add(char a) {
+
+    }
+
+    public void update(char a) {
+
+    }
+
+    NodeChar search(char Character){
+        return null;
+    }
+}
---
+++ src/br/com/company/view/Main.java
@@ -0,0 +1,13 @@
+package br.com.company.view;
+
+import br.com.company.model.BinaryTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        BinaryTree tree = new BinaryTree();
+        tree.add('a',1);
+        tree.update('a');
+    }
+}
